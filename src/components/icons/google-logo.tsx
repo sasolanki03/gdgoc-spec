0f8ec3a8bffdab275b3cf94cddfc819a742c1971
Shivani Solanki <sasolanki03@gmail.com>
2025-10-05 15:10:49 +0000

    logo in not proper , logo is not shown full

--- src/components/icons/google-logo.tsx
+++ src/components/icons/google-logo.tsx
@@ -1,29 +1,19 @@
+
 import type { SVGProps } from 'react';
 
 export function GoogleLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 108 72"
+      viewBox="0 0 43.14 26.57"
       fill="none"
       {...props}
     >
-      <path
-        fill="#EA4335"
-        d="M21.579 50.842 1.284 36l20.295-14.842L36 36l-14.421 14.842Z"
-      />
-      <path
-        fill="#4285F4"
-        d="M36 36 21.579 21.158 1.284 36l20.295 14.842L36 36Z"
-      />
-      <path
-        fill="#34A853"
-        d="M86.421 21.158 106.716 36 86.42 50.842 72 36l14.42-14.842Z"
-      />
-      <path
-        fill="#FBBC04"
-        d="m72 36 14.421 14.842L106.716 36 86.421 21.158 72 36Z"
-      />
+      <path fill="#4285F4" d="M26.57 13.28v.03l-6.4 6.4h-6.89l8.6-8.6-8.6-8.6h6.89l6.4 6.4v.02l2.7-2.7V0H16.14L0 13.28l16.13 13.29h13.14V15.98l-2.7-2.7Z" />
+      <path fill="#0F9D58" d="m16.14 26.57 8.6-8.6-8.6-8.6v17.2Z" />
+      <path fill="#DB4437" d="M16.14 0v17.2l-8.6-8.6L16.14 0Z" />
+      <path fill="#F4B400" d="M43.14 6.89 34.54 15.5v-4.32l-2.5-2.5-2.7-2.7V0h13.8v6.89Z" />
+      <path fill="#DB4437" d="M29.34 8.68v6.82l2.7 2.7V8.68h-2.7Z" />
     </svg>
   );
 }
--- src/components/shared/animated-gdg-logo.tsx
+++ src/components/shared/animated-gdg-logo.tsx
@@ -1,3 +1,4 @@
+
 export function AnimatedGdgLogo() {
   return (
     <div className="flex items-center justify-center">
@@ -18,30 +19,19 @@ export function AnimatedGdgLogo() {
       <svg
         width="120"
         height="120"
-        viewBox="0 0 108 72"
+        viewBox="0 0 43.14 26.57"
         xmlns="http://www.w3.org/2000/svg"
         className="w-24 h-24 md:w-32 md:h-32"
         fill="none"
       >
         <g className="g-left">
-            <path
-                fill="#EA4335"
-                d="M21.579 50.842 1.284 36l20.295-14.842L36 36l-14.421 14.842Z"
-            />
-            <path
-                fill="#4285F4"
-                d="M36 36 21.579 21.158 1.284 36l20.295 14.842L36 36Z"
-            />
+          <path fill="#4285F4" d="M26.57 13.28v.03l-6.4 6.4h-6.89l8.6-8.6-8.6-8.6h6.89l6.4 6.4v.02l2.7-2.7V0H16.14L0 13.28l16.13 13.29h13.14V15.98l-2.7-2.7Z" />
         </g>
         <g className="g-right">
-            <path
-                fill="#34A853"
-                d="M86.421 21.158 106.716 36 86.42 50.842 72 36l14.42-14.842Z"
-            />
-            <path
-                fill="#FBBC04"
-                d="m72 36 14.421 14.842L106.716 36 86.421 21.158 72 36Z"
-            />
+          <path fill="#0F9D58" d="m16.14 26.57 8.6-8.6-8.6-8.6v17.2Z" />
+          <path fill="#DB4437" d="M16.14 0v17.2l-8.6-8.6L16.14 0Z" />
+          <path fill="#F4B400" d="M43.14 6.89 34.54 15.5v-4.32l-2.5-2.5-2.7-2.7V0h13.8v6.89Z" />
+          <path fill="#DB4437" d="M29.34 8.68v6.82l2.7 2.7V8.68h-2.7Z" />
         </g>
       </svg>
     </div>
