99e3a6929c9bdcf048c20189588881e0051b2ea8
Sagar Chauhan <sagarmc2516@gmail.com>
2025-11-03 05:50:55 +0000

    This issue is in client side. at right side showing extra space and at l

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -141,7 +141,7 @@ const SidebarProvider = React.forwardRef<
               } as React.CSSProperties
             }
             className={cn(
-              "group/sidebar-wrapper flex min-h-svh w-full md:has-[[data-variant=inset]]:bg-sidebar",
+              "group/sidebar-wrapper flex min-h-svh md:has-[[data-variant=inset]]:bg-sidebar",
               className
             )}
             ref={ref}
